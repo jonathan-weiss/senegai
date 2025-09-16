ae0529ef50793f86c3ad27f90b64b662c408afe4
Jonathan Weiss <jonathan.weiss@ergon.ch>
2025-09-16 18:04:09 +0200

    Add transloco files

---
+++ client/src/app/app-config.ts
@@ -0,0 +1,31 @@
+import {ApplicationConfig, isDevMode} from "@angular/core";
+import {provideHttpClient} from "@angular/common/http";
+import {provideTransloco} from "@jsverse/transloco";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {PreloadAllModules, provideRouter, withPreloading, withRouterConfig} from "@angular/router";
+import {ROUTES} from "@app/app-routing";
+import {TranslocoHttpLoader} from "@app/transloco-http-loader";
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideHttpClient(),
+        provideTransloco({
+            config: {
+                availableLangs: ['en', 'es'],
+                defaultLang: 'en',
+                // Remove this option if your application doesn't support changing language in runtime.
+                reRenderOnLangChange: true,
+                prodMode: !isDevMode(),
+            },
+            loader: TranslocoHttpLoader,
+        }),
+        provideRouter(
+            ROUTES,
+            withPreloading(PreloadAllModules),
+            withRouterConfig({
+                onSameUrlNavigation: "reload",
+            })
+        ),
+        provideAnimations(),
+    ],
+};
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,21 +2,6 @@
 
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
-import {ROUTES} from "./app/app-routing";
-import {PreloadAllModules, provideRouter, withPreloading, withRouterConfig} from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import {provideAnimations} from "@angular/platform-browser/animations";
+import {appConfig} from "@app/app-config";
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        provideAnimations(),
-        provideHttpClient(),
-        provideRouter(
-            ROUTES,
-            withPreloading(PreloadAllModules),
-            withRouterConfig({
-                onSameUrlNavigation: "reload",
-            })
-        ),
-    ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
