2943457200b35584c622e8000c525b15e7fdcc21
Jonathan Weiss <jonathan.weiss@ergon.ch>
2025-12-23 23:28:24 +0100

    Use autodetection for sealed interfaces in sourceamazing 4.0.0-rc2

--- code-generation/code-generator-data/src/main/kotlin/senegai/codegen/schema/ItemAttributeData.kt
+++ code-generation/code-generator-data/src/main/kotlin/senegai/codegen/schema/ItemAttributeData.kt
@@ -1,10 +1,7 @@
 package senegai.codegen.schema
 
-import org.codeblessing.sourceamazing.schema.api.annotations.References
-
 data class ItemAttributeData(
     val attributeName: String,
     val cardinality: ItemAttributeCardinality,
-    @References([ItemAttributeBuiltInType::class, ItemAttributeEnumType::class, ItemAttributeNestedItemType::class])
     val type: ItemAttributeType,
 )
