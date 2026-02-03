2e0aeff6ea810e09fa0fc0c79a833f7005110877
Jonathan Weiss <jonathan.weiss@ergon.ch>
2026-02-03 16:55:51 +0100

    Add tabs to the author-form-part component

--- client/src/app/author/author-form/author-form-part/author-form-part.component.ts
+++ client/src/app/author/author-form/author-form-part/author-form-part.component.ts
@@ -51,6 +51,7 @@ import {
 import {GenderEnumValues} from "@app/author/gender.enum";
 import {GenderI18nComponent} from "@app/author/gender-i18n/gender-i18n.component";
 import {BooleanInputComponent} from "@app/shared/form-controls/boolean-input/boolean-input.component";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 /* @tt{{{ @slbc  @end-ignore-text @slac }}}@ */
 
@@ -81,6 +82,8 @@ import {BooleanInputComponent} from "@app/shared/form-controls/boolean-input/boo
         AuthorLibraryAwardFormPartComponent,
         GenderI18nComponent,
         BooleanInputComponent,
+        MatTabGroup,
+        MatTab,
         /* @tt{{{ @slbc  @end-ignore-text }}}@ */
     ]
 })
