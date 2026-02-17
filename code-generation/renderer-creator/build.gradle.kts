b64e9958610493b1603e1c82984ad4becc4427f1
Jonathan Weiss <jonathan.weiss@ergon.ch>
2026-02-17 12:35:06 +0100

    Clean renderers before recreating them

--- code-generation/renderer-creator/build.gradle.kts
+++ code-generation/renderer-creator/build.gradle.kts
@@ -8,10 +8,11 @@ dependencies {
     runtimeOnly(libs.typicaltemplate)
 }
 
+val codeGenerationProjectPath = project(":code-generation:code-generator").projectDir
+val targetDirectoryForTemplateRenderer = codeGenerationProjectPath.resolve("src/typicaltemplate-generated/kotlin")
+
 tasks.register<JavaExec>("createTypicalTemplateRenderers") {
     val angularClientProjectPath = project(":client").projectDir
-    val codeGenerationProjectPath = project(":code-generation:code-generator").projectDir
-    val targetDirectoryForTemplateRenderer = codeGenerationProjectPath.resolve("src/typicaltemplate-generated/kotlin")
 
     classpath = sourceSets.main.get().runtimeClasspath
     mainClass.set("senegai.codegen.renderercreator.TypicalTemplateRendererCreatorKt")
@@ -20,4 +21,10 @@ tasks.register<JavaExec>("createTypicalTemplateRenderers") {
         angularClientProjectPath.resolve("src/app").absolutePath, // First argument: Path to the directory within the angular files with tt comments are searched
         targetDirectoryForTemplateRenderer.absolutePath  // Second argument: Path to the base directory where the template renderers are written to
     )
+
+    dependsOn("cleanRenderers")
+}
+
+tasks.register<Delete>("cleanRenderers") {
+    delete(targetDirectoryForTemplateRenderer)
 }
