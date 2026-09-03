1f8c3bbe10240ee8110b5ca1c278438953701675
Jonathan Weiss <jonathan.weiss@ergon.ch>
2026-09-03 20:21:31 +0200

    Fit size of columns of search result table to the size of the window.

--- code-generation/code-generator/src/tavnit-generated/kotlin/senegai/codegen/renderer/angular/EntityResultComponentHtmlRenderer.kt
+++ code-generation/code-generator/src/tavnit-generated/kotlin/senegai/codegen/renderer/angular/EntityResultComponentHtmlRenderer.kt
@@ -23,30 +23,34 @@ object EntityResultComponentHtmlRenderer : UiEntityRenderer {
           |            <ng-container>New ${model.entityName.pascalCase}</ng-container>
           |        </button>
           |    </div>
-          |    <table mat-table [dataSource]="dataSource">
+          |    <div class="${model.entityName.kebabCase}-table-scroll">
+          |        <table mat-table [dataSource]="dataSource">
           |
-          |${ model.searchResultAttributes.joinToString("") { attribute ->  """        <!-- ${attribute.attributeName.pascalCase} Column -->
-              |        <ng-container matColumnDef="${attribute.attributeName.camelCase}">
-              |            <th mat-header-cell *matHeaderCellDef>${attribute.attributeName.pascalCase}</th>
-              |            <td mat-cell *matCellDef="let ${model.entityName.camelCase}">{{ ${model.entityName.camelCase}.${attribute.attributeName.camelCase} }}</td>
-              |        </ng-container>
+          |${ model.searchResultAttributes.joinToString("") { attribute ->  """            <!-- ${attribute.attributeName.pascalCase} Column -->
+              |            <ng-container matColumnDef="${attribute.attributeName.camelCase}">
+              |                <th mat-header-cell *matHeaderCellDef><span class="table-cell-content">${attribute.attributeName.pascalCase}</span></th>
+              |                <td mat-cell *matCellDef="let ${model.entityName.camelCase}">
+              |                    <span class="table-cell-content" [title]="${model.entityName.camelCase}.${attribute.attributeName.camelCase}">{{ ${model.entityName.camelCase}.${attribute.attributeName.camelCase} }}</span>
+              |                </td>
+              |            </ng-container>
               |""" } }
-          |        <!-- Actions Column -->
-          |        <ng-container matColumnDef="actions">
-          |            <th mat-header-cell *matHeaderCellDef>Actions</th>
-          |            <td mat-cell *matCellDef="let ${model.entityName.camelCase}">
-          |                <button mat-icon-button color="primary" (click)="onEdit(${model.entityName.camelCase})">
-          |                    <mat-icon>edit</mat-icon>
-          |                </button>
-          |                <button mat-icon-button color="warn" (click)="onDelete(${model.entityName.camelCase})">
-          |                    <mat-icon>delete</mat-icon>
-          |                </button>
-          |            </td>
-          |        </ng-container>
+          |            <!-- Actions Column -->
+          |            <ng-container matColumnDef="actions" stickyEnd>
+          |                <th mat-header-cell *matHeaderCellDef>Actions</th>
+          |                <td mat-cell *matCellDef="let ${model.entityName.camelCase}">
+          |                    <button mat-icon-button color="primary" (click)="onEdit(${model.entityName.camelCase})">
+          |                        <mat-icon>edit</mat-icon>
+          |                    </button>
+          |                    <button mat-icon-button color="warn" (click)="onDelete(${model.entityName.camelCase})">
+          |                        <mat-icon>delete</mat-icon>
+          |                    </button>
+          |                </td>
+          |            </ng-container>
           |
-          |        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-          |        <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-          |    </table>
+          |            <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          |            <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+          |        </table>
+          |    </div>
           |</div> 
           |
         """.trimMargin(marginPrefix = "|")
